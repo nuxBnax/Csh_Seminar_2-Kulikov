122acf2a20a03149bf16047f1ad49248c69d97c1
Kulikov Alexander <shouter84@yandex.ru>
2023-04-25 23:41:18 +0300

    At last added solution of task_13, that was a HARD one

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -8,22 +8,16 @@
 
 Console.WriteLine("Enter ANY number, PLEASE!");
 int num = Convert.ToInt32(Console.ReadLine());
-// int count = 100;
 int k = 1;
 int find = 1;
 
 if (num < 100) Console.WriteLine("There is NO third number");
-// if (num >= count && num <= (count * 10) - 1) Console.WriteLine("The third number is: " + ((num % (10 * k)) / k));
-// if (num >= 1000 && num <= 9999) Console.WriteLine("The third number is: " + ((num % (10 * k)) / (10 * k)));
-// if (num >= 10000 && num <= 99999) Console.WriteLine("The third number is: " + ((num % (10 * k)) / k));
-if (num >= 100)
-{
-	while (num >= 100 * k && num <= ((1000 * k) - 1)) 
-	{
-		find = (num % (10 * k)) / k;
+
+else
+    while (num >= 100 * k)
+    {
+        find = (num % (10 * k)) / k;
         k = k * 10;
     }
-    
-Console.WriteLine("The third number is: " + (find));
-}
 
+if (num >= 100)	System.Console.WriteLine("The third number is: " + (find));
